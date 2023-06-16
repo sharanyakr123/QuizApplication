6040fa5f41612cb9e674e25f754e01b77506d93e
sharanyakr123 <84069631+sharanyakr123@users.noreply.github.com>
2023-06-16 15:18:51 +0530

    First Commit

---
+++ src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,36 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String[] question ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "What is the name of Android bot?",
+            "When is Android Day celebrated?",
+            "Which is the current trending AI bot",
+            "Which company developed the Java programming language?",
+            "What is the world's largest social networking platform?",
+            "What is the most widely used web browser"
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Bruno","Bubble","Bugdroid","AmazeBot"},
+            {"Oct 13","Aug 30","Apr 30","Nov 5"},
+            {"QuillBot","ChatGPT","ChatBot","Google"},
+            {"Sun Microsystems","Tesla","IBM","Sun Systems"},
+            {"Instagram","Facebook","Twitter","Whats app"},
+            {"Chrome","Mozilla Firefox","Internet Explorer","Yahoo"}
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Notepad",
+            "Bugdroid",
+            "Nov 5",
+            "ChatGPT",
+            "Sun Microsystems",
+            "Facebook",
+            "Chrome"
+
+};}
